9f15d5bb1ffecd0565bec375a406a30accf1ba74
Umar <umar.mohammed.tech@gmail.com>
2023-09-12 13:58:18 +0530

    Updates

---
+++ precache-manifest.2a4d220c377848bab6cbd6007e726026.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2725610457ab59c21fa0",
+    "url": "/TextUtils-React/static/css/main.c2bf082e.chunk.css"
+  },
+  {
+    "revision": "2725610457ab59c21fa0",
+    "url": "/TextUtils-React/static/js/main.27256104.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/TextUtils-React/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/TextUtils-React/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "2591e466b5f270c0c477",
+    "url": "/TextUtils-React/static/js/runtime~main.2591e466.js"
+  },
+  {
+    "revision": "e075370055cf090b801c4835697e90e8",
+    "url": "/TextUtils-React/index.html"
+  }
+];
