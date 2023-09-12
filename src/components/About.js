494e8f60b4b914c9d5b1b2c0cd64d307cc7c90a3
Umar <umar.mohammed.tech@gmail.com>
2023-09-12 14:19:14 +0530

    hiding accordion in about

--- src/components/About.js
+++ src/components/About.js
@@ -21,7 +21,7 @@ export default function About(props) {
                         <strong>Analyze your text</strong>
                         </button>
                     </h2>
-                    <div id="collapseOne" className="accordion-collapse collapse show" data-bs-parent="#accordionExample">
+                    <div id="collapseOne" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
                         <div className="accordion-body">
                             This is the first item's accordion body. It is shown by default, until the collapse plugin adds the appropriate classNamees that we use to style each element. These classNamees control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
                         </div>
